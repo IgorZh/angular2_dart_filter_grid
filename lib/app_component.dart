7f397894938f82e50d26ccf7ddb647bd2f6c6ae3
Igor Zheleznov <igorzheleznov@gmail.com>
2016-08-07 09:58:47 +0300

    small fixes. multisorting is answer but not main

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -5,27 +5,59 @@ import 'user_service.dart';
 import 'dart:async';
 import 'user.dart';
 import 'filter_column_settings.dart';
+import 'sort_column_settings.dart';
 
 @Component(
-      selector: 'my-app',
-      templateUrl: 'app_component.html',
-      directives: const [GridComponent],
-      providers: const [UserService]
-   )
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    directives: const [GridComponent],
+    providers: const [UserService]
+)
 class AppComponent implements OnInit {
-   final UserService _userService;
+  final UserService _userService;
 
-   AppComponent(this._userService);
+  AppComponent(this._userService);
 
-   Future<List<User>> users;
+  Future<List<User>> users;
 
-   List<FilterColumnSettings<User>> filterColumnSettings = [
-      new FilterColumnSettings<User>('gender', 'Gender', (User user) => user.gender, (User a, String b) => a.gender == b),
-      new FilterColumnSettings<User>('department', 'Department', (User user) => user.department, (User a, String b) => a.department == b),
-      new FilterColumnSettings<User>('city', 'City', (User user) => user.address.city, (User a, String b) => a.address.city == b)
-   ];
+  List<FilterColumnSettings<User>> filterColumnSettings = [
+    new FilterColumnSettings<User>(
+        'gender', 'Gender', (User user) => user.gender, (User a, String b) => a
+        .gender == b),
+    new FilterColumnSettings<User>(
+        'department', 'Department', (User user) => user.department, (User a,
+        String b) => a.department == b),
+    new FilterColumnSettings<User>(
+        'city', 'City', (User user) => user.address.city, (User a,
+        String b) => a.address.city == b)
+  ];
 
-   void ngOnInit() {
-      users = _userService.getUsers();
-   }
+  List<SortColumnSettings<User>> sortColumnSettings = [
+    new SortColumnSettings<User>('name',
+        'Name',
+        (User user) => user.name,
+        (User a, User b) => a.name.compareTo(b.name),
+        (User a, User b) => b.name.compareTo(a.name)
+    ),
+    new SortColumnSettings<User>(
+        'age', 'Age', (User user) => user.age, (User a, User b) =>
+        a.age.compareTo(b.age), (User a, User b) => b.age.compareTo(a.age)),
+    new SortColumnSettings<User>(
+        'gender', 'Gender', (User user) => user.gender, (User a, User b) =>
+        a.gender.compareTo(b.gender), (User a, User b) =>
+        b.gender.compareTo(a.gender)),
+    new SortColumnSettings<User>(
+        'department', 'Department', (User user) => user.department, (User a,
+        User b) => a.department.compareTo(b.department), (User a, User b) =>
+        b.department.compareTo(a.department)),
+    new SortColumnSettings<User>(
+        'address', 'Address', (User user) => '${user.address.city}, ${user
+        .address.street}', (User a, User b) =>
+        (a.address.city + a.address.street).compareTo(b.address.city + b.address.street),
+        (User a, User b) => (b.address.city + b.address.street).compareTo(a.address.city + a.address.street))
+  ];
+
+  void ngOnInit() {
+    users = _userService.getUsers();
+  }
 }
--- lib/grid_component.dart
+++ lib/grid_component.dart
@@ -8,9 +8,8 @@ import 'filter_component.dart';
 import 'filter_service.dart';
 import 'sort_service.dart';
 import 'filter_column_settings.dart';
+import 'column_settings.dart';
 import 'sort_order.dart';
-
-
 
 @Component(
     selector: 'i-grid',
@@ -30,19 +29,13 @@ class GridComponent implements OnInit{
   Future<List> input;
   @Input()
   List<FilterColumnSettings> filterColumnSettings;
+  @Input()
+  List<ColumnSettings> columnSettings;
 
   GridComponent(this._filterService, this._sortService)
     : appliedSorters = new Map<String, SortOrder>();
 
   List rows;
-
-  List<SortColumnSettings<User>> columns = [
-    new SortColumnSettings<User>('name', 'Name', (User user) => user.name, (User a, User b) => a.name.compareTo(b.name), (User a, User b) => b.name.compareTo(a.name)),
-    new SortColumnSettings<User>('age', 'Age', (User user) => user.age, (User a, User b) => a.age.compareTo(b.age), (User a, User b) => b.age.compareTo(a.age))
-    //new Column('gender', 'Gender'),
-    //new Column('department', 'department'),
-    //new Column('address', 'Address', (User user) => '${user.address.city} ${user.address.street}')
-  ];
 
   void onSort(SortColumnSettings<User> col) {
     _sortService.toggleSort(col.name);
@@ -56,7 +49,7 @@ class GridComponent implements OnInit{
 
   Future<List> ngOnInit() async {
     _filterService.init(input, filterColumnSettings);
-    _sortService.init(columns);
+    _sortService.init(columnSettings);
     appliedSorters = _sortService.appliedSorters;
     rows = await input;
   }
